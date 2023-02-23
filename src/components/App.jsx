9c4961a50a023262b7cea41ad86b0918af631f6e
Oleg Kropiviansky <oleg.kropiviansky@gmail.com>
2023-02-23 20:22:08 +0200

    feat: module Section

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,32 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import { Section } from './Section/Section';
+import style from './App.module.css';
+
+export class App extends Component {
+  render() {
+    return (
+      <>
+        <div className={style.app}>
+          <Section title="Please leave feedback"></Section>
+          <Section title="Statistics"></Section>
+        </div>
+      </>
+    );
+  }
+}
+// export const App = () => {
+//   return (
+//     <div
+//       style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         color: '#010101',
+//       }}
+//     >
+//       React homework template
+//     </div>
+//   );
+// };
